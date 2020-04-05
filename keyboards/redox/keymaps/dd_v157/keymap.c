69bb018488eb38b07f069af1037de92c16d9fd6b
alucardcy <tzimis12@hotmail.com>
2020-04-05 19:21:09 +0300

    added arrows

--- keyboards/redox/keymaps/dd_v157/keymap.c
+++ keyboards/redox/keymaps/dd_v157/keymap.c
@@ -19,6 +19,8 @@ enum custom_keycodes {
   C_COPY = SAFE_RANGE,
   C_PASTE,
   C_CUT,
+  LINE_ARROW,
+  EQ_ARROW,
   QWERTY,
   QWERTYGMNG
   
@@ -36,6 +38,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         return false;
     case C_CUT:
         SEND_STRING(SS_LCTL("x"));
+        return false;
+    case LINE_ARROW:
+        SEND_STRING("->");
+        return false;
+    case EQ_ARROW:
+        SEND_STRING("=>");
         return false;
         // when custom keycode qwerty is pressed
         // make it default layer etc.....
@@ -110,12 +118,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //   ),
 
 
-	// [_QWERTY] = LAYOUT(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, LT(3,KC_BSLS), KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, TG(1), TG(2), KC_Y,
-  //  KC_U, KC_I, KC_O, KC_P, KC_EQL, MO(2), KC_A, KC_S, KC_D, KC_F, KC_G, KC_LBRC, KC_RBRC, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V,
-  //   KC_B, LT(1,KC_SPC), KC_ENT, KC_HOME, LT(1,KC_END), KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_LGUI, KC_GRV, KC_CAPS, LALT_T(KC_PAST), LCTL_T(KC_MINS), KC_BSPC,
-  //    KC_DEL, KC_ENT, KC_SPC, KC_RCTL, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT),
 
-	[_SYMB] = LAYOUT(KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_TRNS, KC_EXLM, KC_AT, KC_LCBR, KC_RCBR, KC_PIPE, KC_TRNS, KC_TRNS, KC_NO, KC_P7, KC_P8, KC_P9, KC_NO, KC_F12, KC_TRNS, KC_HASH, KC_DLR, KC_LBRC, KC_RBRC, KC_GRV, KC_TRNS, KC_TRNS, KC_NO, KC_P4, KC_P5, KC_P6, KC_NO, KC_NO, KC_TRNS, KC_PERC, KC_CIRC, KC_LPRN, KC_RPRN, KC_TILD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_P1, KC_P2, KC_P3, KC_PPLS, KC_PMNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_P0, KC_P0, KC_PDOT, KC_PAST, KC_PSLS),
+	[_SYMB] = LAYOUT(KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
+   KC_TRNS, KC_EXLM, KC_DLR, KC_LCBR, KC_RCBR, KC_PIPE, KC_TRNS, KC_TRNS, KC_NO, KC_P7, KC_P8, KC_P9, KC_NO, KC_F12,
+   KC_TRNS, KC_HASH, KC_DLR, KC_LBRC, KC_RBRC, KC_GRV, KC_TRNS, KC_TRNS, KC_NO, KC_P4, KC_P5, KC_P6, KC_NO, KC_NO,
+    KC_TRNS, KC_PERC, KC_CIRC, KC_LPRN, KC_RPRN, KC_AMPR, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_P1, KC_P2, KC_P3, KC_PPLS, KC_PMNS,
+     KC_TRNS, KC_TRNS, LINE_ARROW, EQ_ARROW, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_P0, KC_P0, KC_PDOT, KC_PAST, KC_PSLS),
 
   [_NAV] = LAYOUT(
     //┌────────┬────────┬────────┬────────┬────────┬────────┐                                               ┌────────┬────────┬────────┬────────┬────────┬─────────────┐
